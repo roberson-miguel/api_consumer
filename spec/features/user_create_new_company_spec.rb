3096c029ec9f3c6c58ee2b0060efc3aba71b9427
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-01-31 14:10:57 -0300

    WIP Test de Consumo de API
    Usei o Faker para gerar dados aleatórios para persistir no banco remoto

--- spec/features/user_create_new_company_spec.rb
+++ spec/features/user_create_new_company_spec.rb
@@ -3,21 +3,24 @@ require "rails_helper"
 
 feature 'Created a company' do
   scenario "...I can create a company" do
+    name = Faker::Company.name 
+    cnpj = Faker::Number.number(digits: 10)
+    address = Faker::Address.street_address 
+    description = Faker::Lorem.paragraph
     
     visit companies_path
     click_link "Create New Company"
     
-    fill_in 'name', with: 'Campus Code Testes'
-    fill_in 'cnpj', with: '05.372.840999'
-    fill_in 'address', with: 'Alameda Santos, 011'
-    fill_in 'description', with: 'Consultoria e Treinamentos em tecnologia, especializada em Ruby on Rails'
+    fill_in 'name', with: "#{name}" 
+    fill_in 'cnpj', with: "#{cnpj}"
+    fill_in 'address', with: "#{address}"
+    fill_in 'description', with: "#{description}"
     click_button 'save'
 
-    expect(page).to have_content('Campus Code Testes')
-    expect(page).to have_content('05.372.840999')
-    expect(page).to have_content('Alameda Santos, 011')
-    expect(page).to have_content('Consultoria e Treinamentos em tecnologia, especializada em Ruby on Rails')
-
+    expect(page).to have_content("#{name}")
+    expect(page).to have_content("#{cnpj}")
+    expect(page).to have_content("#{address}")
+    expect(page).to have_content("#{description}")
 
   end
 end
--- spec/features/user_edit_company_spec.rb
+++ spec/features/user_edit_company_spec.rb
@@ -3,20 +3,19 @@ require "rails_helper"
 
 feature 'Edited a company' do
   scenario "...I can edit a company" do
+
+    address = Faker::Address.street_address 
+    description = Faker::Lorem.paragraph
     
     visit companies_path
     click_link "Edit", match: :first
     
-    fill_in 'name', with: 'Campus Code Editado'
-    fill_in 'cnpj', with: '05.372.840/0001-07'
-    fill_in 'address', with: 'Alameda Santos, 1293'
-    fill_in 'description', with: 'Consultoria e Treinamentos em tecnologia, especializada em Ruby on Rails'
+    fill_in 'address', with: "#{address}"
+    fill_in 'description', with: "#{description}"
     click_button 'update'
 
-    expect(page).to have_content('Campus Code Editado')
-    expect(page).to have_content('05.372.840/0001-07')
-    expect(page).to have_content('Alameda Santos, 1293')
-    expect(page).to have_content('Consultoria e Treinamentos em tecnologia, especializada em Ruby on Rails')
+    expect(page).to have_content("#{address}")
+    expect(page).to have_content("#{description}")
 
   end
 end
